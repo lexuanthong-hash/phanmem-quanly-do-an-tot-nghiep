dc9f88ce28452f9b31aa7f94ad31f52efd33f14e
Thong <lt33562@gmail.com>
2026-03-19 10:16:48 +0700

    update 2

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306, // Railway thường dùng port khác, ví dụ 23924
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'thesis_management',
